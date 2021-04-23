02b7d7e14d83732d83b50d35c33ed0021db0a73b
adamranieri <adaman94@gmail.com>
2021-04-23 11:28:01 -0400

    added better structuring and requirements

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,19 @@
-from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from features.poms.wiki_home import WikiHomePage
 
 # all setup and teardown functions must go in this file
 #function must be named using behave conventions
+
+#context is a shared object in behave. Any variable attached to context in before_all will be available
+# in the later step implementations
 def before_all(context):
+    driver: WebDriver = webdriver.Chrome('C:\\Users\\AdamRanieri\\Desktop\\drivers\\chromedriver.exe')
+    wiki_home_page = WikiHomePage(driver)
+    context.driver = driver
+    context.wiki_home_page = wiki_home_page
     print("started")
 
 def after_all(context):
+    context.driver.quit()
     print("ended")
--- features/steps/wiki_steps.py
+++ features/steps/wiki_steps.py
@@ -1,49 +1,55 @@
-from behave import given, when, then, fixture
-from behave.runner import Context
-from selenium import webdriver
+from behave import given, when, then
 from selenium.webdriver.chrome.webdriver import WebDriver
+from behave.runner import Context
 from features.poms.wiki_home import WikiHomePage
-
-driver: WebDriver = webdriver.Chrome('C:\\Users\\AdamRanieri\\Desktop\\drivers\\chromedriver.exe')
-wiki_home_page = WikiHomePage(driver)
 
 @given('The Guest is on the Wikipedia Home Page')
 def get_to_wiki_home(context: Context):
+    driver: WebDriver = context.driver # type annotation for intellisense
     driver.get('https://www.wikipedia.org/')
 
 @when('The Guest clicks on English')
 def nav_to_english(context: Context):
+    wiki_home_page: WikiHomePage = context.wiki_home_page
     wiki_home_page.english().click()
 
 @when('The Guest clicks on Spanish')
 def nav_to_english(context: Context):
+    wiki_home_page: WikiHomePage = context.wiki_home_page
     wiki_home_page.espanol().click()
 
 @when('The Guest clicks on Italian')
 def nav_to_english(context: Context):
+    wiki_home_page: WikiHomePage = context.wiki_home_page
     wiki_home_page.italian().click()
 
 @then('The Guest should be on the Spanish Home Page')
 def verify_on_english(context: Context):
+    driver: WebDriver = context.driver
     assert driver.title == 'Wikipedia, la enciclopedia libre'
 
 @then('The Guest should be on the Italian Home Page')
 def verify_on_english(context: Context):
+    driver: WebDriver = context.driver
     assert driver.title == "Wikipedia, l'enciclopedia libera"
 
 @then('The Guest should be on the English Home Page')
 def verify_on_english(context: Context):
+    driver: WebDriver = context.driver
     assert driver.title == 'Wikipedia, the free encyclopedia'
 
 @when('The Guest types {word} in the search bar')
 def type_into_search_bar(context: Context, word: str):
+    wiki_home_page: WikiHomePage = context.wiki_home_page
     wiki_home_page.search_bar().send_keys(word)
 
 @when("The Guest clicks the search button")
 def press_search_button(context: Context):
+    wiki_home_page: WikiHomePage = context.wiki_home_page
     wiki_home_page.search_button().click()
 
 @then("The title should be {title}")
 def verify_title_page(context: Context, title: str):
+    driver: WebDriver = context.driver
     assert driver.title == title
 
